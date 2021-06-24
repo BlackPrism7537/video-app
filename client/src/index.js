be2f4e53ad2417641132131ed4e96cc92e0287b1
ZachBarcoe <z.barcoe@gmail.com>
2021-06-24 22:11:12 +0000

    service worker

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,18 @@ import { ContextProvider } from "./SocketContext";
 import App from "./App";
 import "./styles.css";
 
+if ('serviceWorker' in navigator) {
+    window.addEventListener('load', function () {
+        navigator.serviceWorker.register('/sw.js').then(function (registration) {
+            // Registration was successful
+            console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        }, function (err) {
+            // registration failed :(
+            console.log('ServiceWorker registration failed: ', err);
+        });
+    });
+}
+
 ReactDOM.render(
     <ContextProvider>
         <App />
